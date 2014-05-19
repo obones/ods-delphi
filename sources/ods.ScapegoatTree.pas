125f7691593d31bb9bfedb1d54aa09afc3275d62
obones <obones@free.fr>
2014-05-19 16:51:14 +0200

    Formatting

--- sources/ods.ScapegoatTree.pas
+++ sources/ods.ScapegoatTree.pas
@@ -62,29 +62,29 @@ var
   a: Integer;
   b: Integer;
 begin
-	// first do basic insertion keeping track of depth
-	u := TNode.Create;
-	u.x := x;
-	u.left := nil;
+  // first do basic insertion keeping track of depth
+  u := TNode.Create;
+  u.x := x;
+  u.left := nil;
   u.right := nil;
   u.parent := nil;
-	d := addWithDepth(u);
+  d := addWithDepth(u);
 
-	if (d > log32(q)) then
+  if (d > log32(q)) then
   begin
-		// depth exceeded, find scapegoat
-		w := TNode(u.parent);
-		a := NodeSize(w);
-		b := NodeSize(TNode(w.parent));
-		while 3*a <= 2*b do
+    // depth exceeded, find scapegoat
+    w := TNode(u.parent);
+    a := NodeSize(w);
+    b := NodeSize(TNode(w.parent));
+    while 3*a <= 2*b do
     begin
-			w := TNode(w.parent);
-			a := NodeSize(w);
-			b := NodeSize(TNode(w.parent));
-		end;
-		rebuild(TNode(w.parent));
-	end;
-	Result := d >= 0;
+      w := TNode(w.parent);
+      a := NodeSize(w);
+      b := NodeSize(TNode(w.parent));
+    end;
+    rebuild(TNode(w.parent));
+  end;
+  Result := d >= 0;
 end;
 
 function TScapegoatTree<T, TNode>.addWithDepth(u: TNode): Integer;
@@ -93,48 +93,48 @@ var
   done: Boolean;
   res: Integer;
 begin
-	w := r;
-	if w = nil then
+  w := r;
+  if w = nil then
   begin
-		r := u;
-		Inc(n);
+    r := u;
+    Inc(n);
     Inc(q);
-		Exit(0);
-	end;
-	done := false;
-	Result := 0;
-	repeat
-		res := compare(u.x, w.x);
-		if res < 0 then
+    Exit(0);
+  end;
+  done := false;
+  Result := 0;
+  repeat
+    res := compare(u.x, w.x);
+    if res < 0 then
     begin
-			if w.left = nil then
+      if w.left = nil then
       begin
-				w.left := u;
-				u.parent := w;
-				done := true;
-			end
+        w.left := u;
+        u.parent := w;
+        done := true;
+      end
       else
       begin
-				w := TNode(w.left);
-			end
-		end
+        w := TNode(w.left);
+      end
+    end
     else if res > 0 then
     begin
-			if w.right = nil then
+      if w.right = nil then
       begin
-				w.right := u;
-				u.parent := w;
-				done := true;
-			end;
-			w := TNode(w.right);
-		end
+        w.right := u;
+        u.parent := w;
+        done := true;
+      end;
+      w := TNode(w.right);
+    end
     else
     begin
-			Exit(-1);
-		end;
-		Inc(Result);
-	until done;
-	Inc(n);
+      Exit(-1);
+    end;
+    Inc(Result);
+  until done;
+  Inc(n);
   Inc(q);
 end;
 
@@ -142,34 +142,34 @@ function TScapegoatTree<T, TNode>.buildBalanced(const a: TArray<TNode>; i, ns: I
 var
   m: Integer;
 begin
-	if ns = 0 then
-		Exit(nil);
+  if ns = 0 then
+    Exit(nil);
 
-	m := ns div 2;
-	a[i + m].left := buildBalanced(a, i, m);
-	if a[i + m].left <> nil then
-		a[i + m].left.parent := a[i + m];
-	a[i + m].right := buildBalanced(a, i + m + 1, ns - m - 1);
-	if a[i + m].right <> nil then
-		a[i + m].right.parent := a[i + m];
+  m := ns div 2;
+  a[i + m].left := buildBalanced(a, i, m);
+  if a[i + m].left <> nil then
+    a[i + m].left.parent := a[i + m];
+  a[i + m].right := buildBalanced(a, i + m + 1, ns - m - 1);
+  if a[i + m].right <> nil then
+    a[i + m].right.parent := a[i + m];
 
-	Result := a[i + m];
+  Result := a[i + m];
 end;
 
 function TScapegoatTree<T, TNode>.log32(q: Integer): Integer;
 begin
-	Result := Ceil(log23*Ln(q));
+  Result := Ceil(log23*Ln(q));
 end;
 
 function TScapegoatTree<T, TNode>.packIntoArray(u: TNode; var a: TArray<TNode>; i: Integer): Integer;
 begin
-	if u = nil then
-		Exit(i);
+  if u = nil then
+    Exit(i);
 
-	i := packIntoArray(TNode(u.left), a, i);
-	a[i] := u;
+  i := packIntoArray(TNode(u.left), a, i);
+  a[i] := u;
   Inc(i);
-	Result := packIntoArray(TNode(u.right), a, i);
+  Result := packIntoArray(TNode(u.right), a, i);
 end;
 
 procedure TScapegoatTree<T, TNode>.rebuild(u: TNode);
@@ -178,40 +178,40 @@ var
   p: TNode;
   a: TArray<TNode>;
 begin
-	ns := NodeSize(u);
+  ns := NodeSize(u);
 
-	p := TNode(u.parent);
-	SetLength(a, ns);
-	packIntoArray(u, a, 0);
-	if p = nil then
+  p := TNode(u.parent);
+  SetLength(a, ns);
+  packIntoArray(u, a, 0);
+  if p = nil then
   begin
-		r := buildBalanced(a, 0, ns);
-		r.parent := nil;
-	end
+    r := buildBalanced(a, 0, ns);
+    r.parent := nil;
+  end
   else if TNode(p.right) = u then
   begin
-		p.right := buildBalanced(a, 0, ns);
-		p.right.parent := p;
-	end
+    p.right := buildBalanced(a, 0, ns);
+    p.right.parent := p;
+  end
   else
   begin
-		p.left := buildBalanced(a, 0, ns);
-		p.left.parent := p;
-	end
+    p.left := buildBalanced(a, 0, ns);
+    p.left.parent := p;
+  end
 end;
 
 function TScapegoatTree<T, TNode>.Remove(x: T): Boolean;
 begin
-	if inherited Remove(x) then
+  if inherited Remove(x) then
   begin
-		if 2*n < q then
+    if 2*n < q then
     begin
-			rebuild(r);
-			q := n;
-		end;
-		Exit(true);
-	end;
-	Result := false;
+      rebuild(r);
+      q := n;
+    end;
+    Exit(true);
+  end;
+  Result := false;
 end;
 
 end.
